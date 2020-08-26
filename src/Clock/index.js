304ad21d493cee87793fb9264edecf00600d3348
Amanda Chawinska <amanda.chawinska@spoko.pl>
2020-08-26 18:29:07 +0200

    added clock

---
+++ src/Clock/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useCurrentDate } from "./useCurrentDate";
+import { Wrapper } from "./styled";
+
+const formatDate = (date) => date.toLocaleString(undefined, {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    day: "numeric",
+    month: "long"
+})
+
+export const Clock = () => {
+    const date = useCurrentDate();
+    
+
+    return (
+        <Wrapper>
+            Dzisiaj jest
+            {" "}
+            {formatDate(date)}
+        </Wrapper>
+    )
+};
