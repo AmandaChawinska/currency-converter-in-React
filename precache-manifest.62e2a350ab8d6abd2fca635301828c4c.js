3d192801d9f21726e20c13d66d5afa9045583645
Amanda Chawinska <amanda.chawinska@spoko.pl>
2020-08-26 18:40:30 +0200

    Updates

---
+++ precache-manifest.62e2a350ab8d6abd2fca635301828c4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501cf32dab4a87ce7bbd47671ae605f8",
+    "url": "/currency-converter-in-React/index.html"
+  },
+  {
+    "revision": "caef60db3461c303b06d",
+    "url": "/currency-converter-in-React/static/js/2.6ce57441.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/currency-converter-in-React/static/js/2.6ce57441.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484fb135749ff4005324",
+    "url": "/currency-converter-in-React/static/js/main.a34e4ed9.chunk.js"
+  },
+  {
+    "revision": "5c8dae39b108498d2501",
+    "url": "/currency-converter-in-React/static/js/runtime-main.88761c80.js"
+  },
+  {
+    "revision": "25f19c869bcdc96de7c409aad2e369fa",
+    "url": "/currency-converter-in-React/static/media/background.25f19c86.jpg"
+  }
+]);
