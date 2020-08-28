e7d7495e1ef22484dfc2a9477727c99004b00e69
Amanda Chawinska <amanda.chawinska@spoko.pl>
2020-08-28 19:56:27 +0200

    added react fragment to Result

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { StyledResult } from "./styled";
 
 const Result = ({ result }) => (
-    <p>
+    <>
         <StyledResult> Otrzymana kwota: {result}</StyledResult>
-    </p>
+    </>
 
 );
 
