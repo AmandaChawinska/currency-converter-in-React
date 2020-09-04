73c60486fcd36a84228347c91d9fe89b1d1c14e0
Amanda Chawinska <amanda.chawinska@spoko.pl>
2020-09-04 12:09:40 +0200

    added Loading,Failure, Footer and useRatesData

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 import Result from "./Result";
 import { Clock } from '../Clock';
-import { currencies } from "../currencies";
 import {
   StyledFieldset,
   StyledLegend,
   StyledLabelText,
   StyledButton,
+  Loading,
+  Failure,
+  Footer,
 } from "./styled";
+
+import { useRatesData } from "./useRatesData";
+
 
 
 const Form = ({ calculateResult, setResult, result }) => {
   const [amount, setAmount] = useState("");
   const [changeFrom, setChangeFrom] = useState("PLN");
   const [changeOn, setChangeOn] = useState("EUR");
+  const ratesData = useRatesData();
 
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -30,82 +36,100 @@ const Form = ({ calculateResult, setResult, result }) => {
 
   return (
     <form onSubmit={onFormSubmit} onReset={onFormReset} >
-   
-      <StyledFieldset>
-      <Clock/>
-        <StyledLegend>Wymiana waluty</StyledLegend>
-
-        <p>
-          <label>
-            <StyledLabelText>Wybierz wartość*:</StyledLabelText>
-            <input
-              type="number"
-              name="amount"
-              min="0"
-              step="0.01"
-              required
-              autoFocus
-              value={amount}
-              onChange={({ target }) => setAmount(target.value)}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            <StyledLabelText>Posiadam walutę:</StyledLabelText>
-            <select
-              value={changeFrom}
-              onChange={({ target }) => setChangeFrom(target.value)}
-              required
-            >
-              {currencies.map((changeFrom => (
-                 <option
-                 key={changeFrom.name}
-                 value={changeFrom.name}
-                 >
-                     {changeFrom.name}
-                 </option>
-
-              )))}
-
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            <StyledLabelText>Chcę otrzymać:</StyledLabelText>
-            <select
-              value={changeOn}
-              onChange={({ target }) => setChangeOn(target.value)}
-              required
-            >
-              {currencies.map((changeOn => (
-                 <option
-                 key={changeOn.name}
-                 value={changeOn.name}
-                 >
-                     {changeOn.name}
-                 </option>
-
-              )))}
-              
+      {ratesData.state === "loading"
+        ? (
+          <Loading>
+            Trwa ładowanie kursów walut z Europejskiego Banku Centralnego. Proszę czekać...
+          </Loading>
+        )
+        : (
+          ratesData.state === "error" ? (
             
+            <Failure>
+              hmm..Coś poszło nie tak. Sprawdź, czy masz połączenie z internetem.
+            </Failure>
+          ) : (
+             
+                <StyledFieldset>
+                  <Clock />
+                  <StyledLegend>Wymiana waluty</StyledLegend>
 
-            </select>
-          </label>
-        </p>
+                  <p>
+                    <label>
+                      <StyledLabelText>Wybierz wartość*:</StyledLabelText>
+                      <input
+                        type="number"
+                        name="amount"
+                        min="0"
+                        step="0.01"
+                        required
+                        autoFocus
+                        value={amount}
+                        onChange={({ target }) => setAmount(target.value)}
+                      />
+                    </label>
+                  </p>
+                  <p>
+                    <label>
+                      <StyledLabelText>Posiadam walutę:</StyledLabelText>
+                      <select
+                        value={changeFrom}
+                        onChange={({ target }) => setChangeFrom(target.value)}
+                        required
+                      >
+                        {Object.keys(ratesData.rates).map(((changeFrom) => (
+                          <option
+                            key={changeFrom}
+                            value={changeFrom}
+                          >
+                            {changeFrom}
+                          </option>
 
-        <p>
-          <StyledButton>Przelicz</StyledButton>
-       
-          <StyledButton type="reset">
-            Od nowa
+                        )))}
+
+                      </select>
+                    </label>
+                  </p>
+                  <p>
+                    <label>
+                      <StyledLabelText>Chcę otrzymać:</StyledLabelText>
+                      <select
+                        value={changeOn}
+                        onChange={({ target }) => setChangeOn(target.value)}
+                        required
+                      >
+                        {Object.keys(ratesData.rates).map(((changeOn) => (
+                          <option
+                            key={changeOn}
+                            value={changeOn}
+                          >
+                            {changeOn}
+                          </option>
+
+                        )))}
+
+
+
+                      </select>
+                    </label>
+                  </p>
+
+                  <p>
+                    <StyledButton>Przelicz</StyledButton>
+
+                    <StyledButton type="reset">
+                      Od nowa
           </StyledButton>
-        </p>
-      
-        <Result result={result}/>
-      </StyledFieldset>
+                  </p>
 
+                  <Result result={result} />
+                  <Footer> Pole oznaczone * jest obowiązkowe.
+      Kurs walut z dnia {ratesData.date}.</Footer>
+                </StyledFieldset>
+              
+            )
+        )}
+        
     </form>
 
 
