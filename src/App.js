18b6c873471855fe50b2bf20eaf4cbafa57a10db
Amanda Chawinska <amanda.chawinska@spoko.pl>
2020-09-04 12:08:11 +0200

    added useRatesData and changed calculateResult

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { useState } from 'react';
 import Container from './container';
 import Header from './Header';
-import Footer from './Footer';
 import Form from './Form';
-
-
+import { useRatesData } from './Form/useRatesData';
 
 
 function App() {
   const [result, setResult] = useState("");
+  const ratesData = useRatesData();
 
-  const rates = {
-    PLN: 1,
-    EUR: 4.4532,
-    USD: 3.9395,
-    GBP: 4.9256,
-  };
+
   const calculateResult = (changeFrom, changeOn, amount) => {
-    const result = (amount * rates[changeFrom]) / rates[changeOn];
+    const result = (amount * ratesData.rates[changeFrom]) / ratesData.rates[changeOn];
 
 
     setResult(`${result.toFixed(2)}`);
@@ -30,8 +24,7 @@ function App() {
         <Header title="Internetowy przelicznik walut &Prime;ItakCięNieStać&Prime;😈" />
         <Form calculateResult={calculateResult} setResult={setResult}
           result={result} />
-        <Footer title="Pole oznaczone * jest obowiązkowe. 
-      Kurs walut z dnia 25.06.2020 r."></Footer>
+      
 
       </Container >
     </>
